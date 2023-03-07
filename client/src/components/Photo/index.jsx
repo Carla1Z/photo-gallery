656644d4e5cf0240ae3bb088f657929fe679ebb9
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-07 19:18:53 -0300

    Photo

--- client/src/components/Photo/index.jsx
+++ client/src/components/Photo/index.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { GiChainedHeart } from "react-icons/gi";
 
-export const Photo = () => {
+export const Photo = ({ url, name, date = "Tue Mar 07 2023 18:40:51 GMT-0300" }, tags) => {
+  const photoDate = new Date(date);
+
+  const addToFav = () => {
+    console.log("Add to favorites");
+  }
+
+  const deletePhoto = () => {
+    console.log("Photo deleted");
+  }
+
   return (
-    <div>Photo</div>
-  )
-}
+    <div>
+      <img src={url} alt={name} />
+      <p>{name}</p>
+      <p>{photoDate.getFullYear()}</p>
+      <div>
+        <button onClick={deletePhoto}>
+          <RiDeleteBin5Fill />
+        </button>
+        <button onClick={addToFav}>
+          <GiChainedHeart />
+        </button>
+      </div>
+      <ul>
+        {tags.map((tag) => (
+          <span>{tag}</span>
+        ))}
+      </ul>
+    </div>
+  );
+};
