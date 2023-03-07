213905d0cea38404ce4bd6a7d76506b5d267b5cf
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-07 18:32:12 -0300

    search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
+import { SearchBar } from "./components/SearchBar";
+import "./GlobalStyles.css"
+
 function App() {
   return (
     <div>
+      <SearchBar />
       <h1>Photo Gallery📸</h1>
     </div>
   );
--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
 
 export const SearchBar = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = e.target.input.value;
+    console.log(value);
+    // setSearch
+  };
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      | <input type="text" placeholder="Search some photos..." name="input" />
+      <button type="submit">
+        <BiSearchAlt />
+      </button>
+    </form>
+  );
+};
