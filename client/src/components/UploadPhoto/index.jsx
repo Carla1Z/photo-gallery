7ebd35a5862bf592a125646b62efbae5d3dcd811
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-08 16:22:12 -0300

    implemento cloudinary

--- client/src/components/UploadPhoto/index.jsx
+++ client/src/components/UploadPhoto/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export const UploadPhoto = () => {
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const img = e.target.imgToUpload.files[0]
-
-    console.log(img);
-  }
+    e.preventDefault();
+    const formData = new FormData();
+    const img = e.target.imgToUpload.files[0];
+    formData.append("file", img);
+    formData.append("tags", ["tractor"]);
+    formData.append("public_id", "fancy_tractor");
+    formData.append("cloud_name", "dzwzqaesj");
+    formData.append("upload_preset", "PhotoGallery");
+    fetch("https://api.cloudinary.com/v1_1/dzwzqaesj/image/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="file" name='imgToUpload' />
-      <button type='submit'>Enviar</button>
+      <input type="file" name="imgToUpload" />
+      <button type="submit">Enviar</button>
     </form>
-  )
-}
+  );
+};
